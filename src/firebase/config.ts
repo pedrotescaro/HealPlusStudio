14e39974e61d029c812c1bfc9c9eb351bb432ab8
Pedro Tescaro <pedroatescaro@gmail.com>
2025-09-25 14:44:05 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-7562309280-b21b5",
+  "appId": "1:228714600401:web:d548983b35c97975c072f0",
+  "apiKey": "AIzaSyBCNHtTKi9k_JiN9W-sUImLpGQqVtO1Zz8",
+  "authDomain": "studio-7562309280-b21b5.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "228714600401"
+};
